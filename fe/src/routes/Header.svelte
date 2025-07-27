c13a386ee04cae1b791c6a48d6e02bf803ae826f
theareph <192461522+theareph@users.noreply.github.com>
2025-07-27 12:19:39 +0000

    add simple fe

---
+++ fe/src/routes/Header.svelte
@@ -0,0 +1,16 @@
+<script>
+    let location = $state("loading...")
+    let condition = $state("loading...")
+    let temp_c = $state("loading...")
+	import { onMount } from 'svelte';
+
+	onMount(async () => {
+        const response = await fetch("http://localhost:8000/api/weather/")
+        const json = await response.json();
+        location = json.location
+        condition = json.condition
+        temp_c = json.temp_c
+
+	});
+</script>
+<h3>You are from {location}. The weather is {condition}, and the temperature is {temp_c}</h3>

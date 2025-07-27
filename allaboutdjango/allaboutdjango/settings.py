8d479def56b0d1245724e3f2b8f6c4aab6779298
theareph <192461522+theareph@users.noreply.github.com>
2025-07-27 11:05:33 +0000

    add frontend

--- allaboutdjango/allaboutdjango/settings.py
+++ allaboutdjango/allaboutdjango/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "core",
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
@@ -121,3 +122,9 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    )
+} 

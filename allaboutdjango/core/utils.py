90e6b135fa179ffed0619dd57b62c7fec5a749a0
theareph <192461522+theareph@users.noreply.github.com>
2025-07-27 10:26:33 +0000

    add caching

--- allaboutdjango/core/utils.py
+++ allaboutdjango/core/utils.py
@@ -4,7 +4,17 @@ import typing as t
 import requests
 
 
+from django.core.cache import cache
+
 def get_weather(ip: str) -> dict[str, t.Any]:
     key = os.environ.get("WEATHERAPI_KEY")
+    if not key:
+        raise ValueError("env var WEATHERAPI_KEY not set")
+    cache_key = f"weatherapi-cache:{ip}"
+    if cache.get(cache_key):
+        return cache.get(cache_key)
+
     resp = requests.get(f"http://api.weatherapi.com/v1/current.json?key={key}&q={ip}")
-    return resp.json()
+    result = resp.json()
+    cache.set(cache_key, result, 3600)
+    return result
--- allaboutdjango/core/views.py
+++ allaboutdjango/core/views.py
@@ -1,6 +1,6 @@
 import json
 
-from django.http import HttpResponse
+from django.http.response import JsonResponse
 from django.shortcuts import render
 from ipware import get_client_ip
 
@@ -8,13 +8,16 @@ from . import utils
 
 
 # Create your views here.
+
 def weather(request):
     ip, is_routable = get_client_ip(request)
+    ip, is_routable = "1.1.1.1", True
     if is_routable is False:
-        return HttpResponse("accessing from local ip")
+        return JsonResponse({"error": "access from local ip"})
     data = utils.get_weather(ip)
     location = f"{data['location']['name']}, {data['location']['region']}, {data['location']['country']}"
     temp_c = data["current"]["temp_c"]
     condition = data["current"]["condition"]["text"]
-    report= f"Weather report for {location}: {temp_c}C {condition}"
-    return HttpResponse(report)
+    return JsonResponse(
+        {"location": location, "temp_c": temp_c, "condition": condition, "original_data": data},
+    )

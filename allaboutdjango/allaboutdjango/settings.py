2a957476355646cd2f6b20f4be971ca91cb873f7
theareph <192461522+theareph@users.noreply.github.com>
2025-07-30 07:15:03 +0000

    add secret key configration

--- allaboutdjango/allaboutdjango/settings.py
+++ allaboutdjango/allaboutdjango/settings.py
@@ -22,8 +22,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure--(lb$mho2b4h13f+b)@!uewepz*3ev^eb1bs#s&(0sp(tf#qeh"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -31,6 +29,9 @@ DEBUG = True
 
 ENV_NAME = ".env.development" if DEBUG else ".env"
 env.read_env(ENV_NAME)
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = env("SECRET_KEY", "django-insecure--(lb$mho2b4h13f+b)@!uewepz*3ev^eb1bs#s&(0sp(tf#qeh")
 ALLOWED_HOSTS = []
 
 

0dd1e02e20a81a35e2e9436fda06d4455d42a1d4
theareph <192461522+theareph@users.noreply.github.com>
2025-08-18 03:46:36 +0000

    more robust IsAdminOrReadonly permission

--- allaboutdjango/core/permissions.py
+++ allaboutdjango/core/permissions.py
@@ -6,4 +6,6 @@ class IsAdminOrReadonly(permissions.BasePermission):
     def has_permission(self, request, view):
         if request.method in permissions.SAFE_METHODS:
             return True
+        if not getattr(request, "user", None,) or not getattr(request.user, "is_admin", None):
+            return False
         return bool(request.user.is_admin)

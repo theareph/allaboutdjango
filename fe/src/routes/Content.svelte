441f2469e08bb2685fc3e70d44544d8b1f6d2272
theareph <192461522+theareph@users.noreply.github.com>
2025-08-18 03:40:46 +0000

    add books frontend

--- fe/src/routes/Content.svelte
+++ fe/src/routes/Content.svelte
@@ -1,12 +1,20 @@
 <script>
     import {PUBLIC_API_BASE_URL} from "$env/static/public"
     import { onMount } from "svelte";
+    const contentMeta = $state({
+        "currentContent": "devlogs",
+    })
     const devlogs = $state([])
+    const books = $state([])
+    const booksMeta = $state({
+        "next": null,
+        "previous": null,
+    })
     const devlogMeta = $state({
         "next": null,
         "previous": null,
     })
-    async function populate_devlogs (url) {
+    async function populateDevlogs (url) {
         devlogMeta.next = null
         devlogMeta.previous = null
         devlogs.splice(0, devlogs.length)
@@ -19,13 +27,34 @@
         devlogMeta.previous = data.previous
 
     }
+    async function populateBooks (url) {
+        booksMeta.next = null
+        booksMeta.previous = null
+        books.splice(0, books.length)
+        const response = await fetch(url)
+        const data = await response.json()
+        data.results.forEach(
+            item => books.push(item)
+        )
+        booksMeta.next = data.next
+        booksMeta.previous = data.previous
+
+    }
+
     onMount(async () => {
-        await populate_devlogs(`${PUBLIC_API_BASE_URL}/api/devlogs/`)
+        await populateDevlogs(`${PUBLIC_API_BASE_URL}/api/devlogs/`)
+        await populateBooks(`${PUBLIC_API_BASE_URL}/api/books/`)
 
     })
 
 </script>
-
+<p>
+    <a href="#" onclick={() => contentMeta.currentContent = "devlogs"}>Devlogs</a>
+    &nbsp;
+    &nbsp;
+    <a href="#" onclick={() => contentMeta.currentContent = "books"}>Books</a>
+</p>
+{#if contentMeta.currentContent === "devlogs"}
 <div id="devlogs">
     <h1>Devlog</h1>
     {#each devlogs as log}
@@ -38,13 +67,32 @@
     </div>
     {/each}
     {#if devlogMeta.next}
-    <button onclick={async () => {await populate_devlogs(devlogMeta.next)}}>Next Page</button>
+    <button onclick={async () => {await populateDevlogs(devlogMeta.next)}}>Next Page</button>
     {/if}
     {#if devlogMeta.previous}
-    <button onclick={async () => {await populate_devlogs(devlogMeta.previous)}}>Previous Page</button>
+    <button onclick={async () => {await populateDevlogs(devlogMeta.previous)}}>Previous Page</button>
     {/if}
-
 </div>
+{/if}
+{#if contentMeta.currentContent === "books"}
+<div id="books">
+    <h1>Books</h1>
+    {#each books as book}
+    <div class="book-entry">
+    <h3>{book.name}</h3>
+    Author: {book.author}
+    ISBN: {book.isbn}
+    </div>
+    {/each}
+    {#if booksMeta.next}
+    <button onclick={async () => {await populateBooks(booksMeta.next)}}>Next Page</button>
+    {/if}
+    {#if booksMeta.previous}
+    <button onclick={async () => {await populateBooks(booksMeta.previous)}}>Previous Page</button>
+    {/if}
+</div>
+{/if}
+
 <style>
     #devlogs {
         border: 1px dotted;
@@ -52,4 +100,8 @@
     .devlog-entry {
         border: 1px solid #000;
     }
+    .book-entry {
+        border: 1px solid #000;
+    }
+
 </style>

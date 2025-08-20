cb9cde3eb8ef06061cce8cd004959c397bbcbadd
theareph <192461522+theareph@users.noreply.github.com>
2025-08-20 08:02:00 +0000

    fe: add books and devlog posts

--- fe/app/(root)/page.js
+++ fe/app/(root)/page.js
@@ -1,8 +1,18 @@
-import Weather from "../components/Weather";
+'use client'
+import { useState } from "react";
+import DevLogs from "../components/DevLogs";
+import Books from "../components/Books";
+
 export default function Home() {
+  
+  const [content, setContent] = useState("devlogs")
   return (
     <>
-    <div>hello world</div>
+    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={() => setContent("devlogs")}>Devlogs</button>
+    &nbsp;&nbsp;
+    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={() => setContent("books")}>Books</button>
+    {content === "devlogs" && <><div>Dev logs</div><DevLogs/></>}
+    {content === "books" && <><div>Books</div><Books/></>}
     </>
   );
 }
---
+++ fe/app/components/Books.jsx
@@ -0,0 +1,43 @@
+'use client'
+import React, { useEffect, useState } from 'react'
+
+const Books = () => {
+    const [nextPageURL, setNextPageURL] = useState(null)
+    const [previousPageURL, setPreviousPageURL] = useState(null)
+    const [books, setBooks] = useState([])
+
+    const populateBooks = async (url) => {
+        const resp = await fetch(url)
+        const data = await resp.json()
+        setBooks(data.results)
+        setNextPageURL(data.next)
+        setPreviousPageURL(data.previous)
+    }
+    useEffect(() => {
+        const initBooks = async () => {
+            await populateBooks("http://localhost:8000/api/books/")       
+        }
+        initBooks()
+    }, [])
+    const bookList = books.map(
+        (book, index) => (
+            <div key={index} className='border-solid border-1'>
+            <span className='font-bold'>{book.name}</span>
+            <br/>
+            <span>by {book.author}</span>
+            <br/>
+
+            <span>ISBN: {book.isbn}</span>
+            </div>
+        )
+    )
+  return (
+      <>
+      {bookList}
+      {nextPageURL && (<><button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded' onClick={async () => await populateBooks(nextPageURL)}>Next Page</button><br/></>)}
+      {previousPageURL && (<><button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded' onClick={async () => await populateBooks(previousPageURL)}>Previous Page</button><br/></>)}
+    </>
+  )
+}
+
+export default Books
